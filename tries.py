775ddb08c16d66b07eaaabf370e7677d0c341559
grebnesorbocaj <bocajgrebnesor@gmail.com>
2022-09-23 14:52:36 -0600

    dev: trie class with functions
    - addWord(word): add single word
    - addManyWords(words): add words from list
    - addSenttenceWords(sentence): addManyWords(sentence.split(" "))
    - searchWord(pre): search whether word with prefix exists
    - listWords(prefix=""): list words, optionally those starting with prefix

---
+++ tries.py
@@ -0,0 +1,74 @@
+def charPosition(c):
+    c = c.lower()
+    return ord(c) - ord("a")
+
+def positionChar(i):
+    az = "abcdefghijklmnopqrstuvwxyz"
+    return az[i]
+
+class TrieNode:
+    def __init__(self):
+        self.children = [None] * 26
+        self.end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word):
+        curr = self.root
+        for char in word:
+            charPos = charPosition(char)
+            if curr.children[charPos] == None:
+                curr.children[charPos] = TrieNode()
+            curr = curr.children[charPos]
+        curr.end = True
+
+    def addManyWords(self, words):
+        for word in words:
+            self.addWord(word)
+        
+    def addSentenceWords(self, sentence):
+        words = sentence.split(" ")
+        self.addManyWords(words)
+
+
+    def searchWord(self, pre):
+        curr = self.root
+        for char in pre:
+            charPos = charPosition(char)
+            if curr.children[charPos] == None:
+                return False
+            curr = curr.children[charPos]
+        return True
+
+    def listWords(self, prefix = ""):
+        words = []
+        curr = self.root
+        def dfs(currChar, currStr):
+            nonlocal words
+            for i, node in enumerate(currChar.children):
+                if node != None:
+                    char = positionChar(i)
+                    if node.end == True:
+                        words.append(currStr+char)
+                    dfs(node, currStr+char)
+        
+        if prefix != "":
+            for c in prefix:
+                charPos = charPosition(c)
+                curr = curr.children[charPos]
+            if curr.end == True:
+                words.append(prefix)
+
+        dfs(curr, prefix)
+
+        
+        if not words:
+            if prefix != "":
+                return [f"No words with given prefix: {prefix}"]
+            else:
+                return [f"No words in trie as of yet"]
+        else:
+            return words
+        
